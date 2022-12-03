dfe1dd1a8a72aa356ff6a57cff000e61d924473b
Ramon Paolo Maram <ramonpaolomaran12@gmail.com>
2022-12-02 21:44:16 -0300

    feat: add route to create and delete user and populate database with faker.js

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,50 @@
 import express from 'express'
 import pg from 'pg'
 
+import { faker } from '@faker-js/faker'
+
 const app = express()
 
 app.use(express.json())
 
-const database = new pg.Client({password: 'postgresql', user: 'postgresql', host: 'localhost'});
+const database = new pg.Client({ password: 'postgresql', user: 'postgresql', host: 'localhost' });
 
 (async () => {
     await database.connect()
     await database.query('CREATE TABLE IF NOT EXISTS users (name varchar(50));')
+    for (let i = 0; i <= 10; i++) {
+        const name = faker.name.firstName()
+        await database.query(`INSERT INTO "users" VALUES ('${name}')`)
+    }
 })()
 
 type Country = 'Brazil' | 'United States';
 
-interface IUser{
+interface IUser {
     name: string;
     age?: string;
     country: Country;
 }
 
-app.get('/users', async (req, res) => {
-    const limit = parseInt(req.body.limit) || 10;
+app.route('/users').get(async (req, res) => {
+    const limit = parseInt(req.body.limit) || 100;
 
     const resultQuery = await database.query(`SELECT * FROM "users" LIMIT ${limit}`)
     const rows: IUser[] = resultQuery.rows
-    
-    for(const user of rows){
-        console.log(user.name)
-    }
-    
+
     res.json(rows)
+}).post(async (req, res) => {
+    const user = req.body as IUser
+
+    const resultQuery = await database.query(`INSERT INTO "users" VALUES ('${user.name}')`)
+
+    res.status(201).json(resultQuery)
+}).delete(async (req, res) => {
+    const user = req.body as IUser
+
+    const resultQuery = await database.query(`DELETE FROM "users" WHERE name = '${user.name}'`)
+
+    res.status(200).json(resultQuery)
 })
 
 app.listen(3000)
